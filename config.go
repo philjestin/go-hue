21dbc377f5168c2666806f2db10b1f3d1d783896
Philip Middleton <middleton.philip.pm@gmail.com>
2021-03-27 13:36:58 -0600

    Updated to get and set config / read all data so far

--- config.go
+++ config.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"net"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 )
 
 // Configuration defines whats required to communicate
@@ -14,9 +15,24 @@ type Configuration struct {
 	HueUser string
 }
 
-func config(hueIp net.IP, hueUser string) {
+func readFromConfig() Configuration {
+	file, err := ioutil.ReadFile("./config/config.json")
+
+	if err != nil {
+		log.Fatalln("'go-hue config --ip <your-bridges-ip> --user-token <bridge-generated-token>' must be ran first.")
+	}
+
+	data := Configuration{}
+
+	_ = json.Unmarshal([]byte(file), &data)
+
+	return data
+}
+
+// config is used to set the configuration settings in your ./config/config.json file
+func config(hueIP net.IP, hueUser string) {
 	data := Configuration{
-		HueIP: hueIp,
+		HueIP:   hueIP,
 		HueUser: hueUser,
 	}
 
@@ -30,4 +46,11 @@ func config(hueIp net.IP, hueUser string) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func printConfig() {
+	data := readFromConfig()
+
+	fmt.Printf("Hue Bridge IP: %v\n", data.HueIP)
+	fmt.Printf("Hue Bridge User Token: %v\n", data.HueUser)
 }
