663c08302adf7caf770ae63300507acff1ea01aa
Philip Middleton <middleton.philip.pm@gmail.com>
2021-03-28 19:44:05 -0600

    Start breaking up the packages

--- communicate.go
+++ communicate.go
@@ -1,54 +1,21 @@
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
-	"net/http"
+
+	"github.com/philjestin/go-hue/utils"
 )
-
-// On to turn on a item the request body
-type On struct {
-	On bool `json:"on"`
-}
-
-func getClient() *http.Client {
-	// No valid TLS Cert on Philips Hue Bridge
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-
-	return client
-}
-
-func readResponseBody(resp *http.Response, err error) []byte {
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	return body
-}
 
 // getLightState retrieves light state from the Hue Bridge
 func getLightState(hueIP net.IP, hueUser string) {
 	// State url
 	stateURL := fmt.Sprintf("https://%s/api/%s/lights", hueIP, hueUser)
-	client := getClient()
+	client := utils.GetClient()
 	resp, err := client.Get(stateURL)
 
-	res := readResponseBody(resp, err)
+	res := utils.ReadResponseBody(resp, err)
 
 	//Convert the body to type string
 	sb := string(res)
@@ -59,9 +26,27 @@ func getLightState(hueIP net.IP, hueUser string) {
 func getGroups(hueIP net.IP, hueUser string) {
 	// Groups URL
 	groupsURL := fmt.Sprintf("https://%s/api/%s/groups", hueIP, hueUser)
-	client := getClient()
+	client := utils.GetClient()
 	resp, err := client.Get(groupsURL)
-	res := readResponseBody(resp, err)
+	res := utils.ReadResponseBody(resp, err)
+
+	//Convert the body to type string
+	sb := string(res)
+	log.Printf(sb)
+}
+
+func toggleLight(hueIP net.IP, hueUser string, group string, onValue bool) {
+	on := utils.On{
+		On: onValue,
+	}
+
+	update := utils.UpdateObject{
+		URL:    fmt.Sprintf("https://%s/api/%s/lights/%s/action", hueIP, hueUser, group),
+		Client: utils.GetClient(),
+		On:     on,
+	}
+
+	res := utils.UpdateItem(update)
 
 	//Convert the body to type string
 	sb := string(res)
@@ -69,31 +54,17 @@ func getGroups(hueIP net.IP, hueUser string) {
 }
 
 func toggleGroup(hueIP net.IP, hueUser string, group string, onValue bool) {
-	// Groups URL
-	groupsURL := fmt.Sprintf("https://%s/api/%s/groups/%s/action", hueIP, hueUser, group)
-	client := getClient()
-
-	on := On{
+	on := utils.On{
 		On: onValue,
 	}
 
-	// marshal User to json
-	json, err := json.Marshal(on)
-	if err != nil {
-		log.Fatalln(err)
+	update := utils.UpdateObject{
+		URL:    fmt.Sprintf("https://%s/api/%s/groups/%s/action", hueIP, hueUser, group),
+		Client: utils.GetClient(),
+		On:     on,
 	}
 
-	// set the HTTP method, url, and request body
-	req, err := http.NewRequest(http.MethodPut, groupsURL, bytes.NewBuffer(json))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// set the request header Content-Type for json
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	resp, err := client.Do(req)
-
-	res := readResponseBody(resp, err)
+	res := utils.UpdateItem(update)
 	//Convert the body to type string
 	sb := string(res)
 	log.Printf(sb)
